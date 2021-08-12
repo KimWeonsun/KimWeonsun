ad8840a006c16e3a15a5d6a513311e0afcad624c
KimWeonsun <88815212+KimWeonsun@users.noreply.github.com>
2021-08-12 17:35:28 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,9 +5,10 @@ int main()
    char *p1, *p2;
   
    p1="Hello";
-   #p2=1000;
+   p2=1000;
    
    printf("Hello World. \n");
-   #printf("p1: %s, p2: %s \n", p1, p2)
+   printf("p1: %s, p2: %s \n", p1, p2)
+      
    return 0;
 }
